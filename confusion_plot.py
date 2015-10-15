afabdaa55b7db9df4c6a6d61daa34f4cfcc5a1e3
Thomas Brawner <thomas@Thomass-MacBook-Pro.local>
2015-10-15 10:10:57 -0600

    add docstrings

--- confusion_plot.py
+++ confusion_plot.py
@@ -1,5 +1,5 @@
-## ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– ##
-## ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– ##
+## ----------------------------------------------------------------------------------------------- ##
+## ----------------------------------------------------------------------------------------------- ##
 
 import matplotlib.pyplot as plt 
 import numpy as np 
@@ -7,10 +7,14 @@ import pandas as pd
 import seaborn as sns 
 from sklearn.metrics import confusion_matrix
 
-## ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– ##
+## ----------------------------------------------------------------------------------------------- ##
 
 def format_confusion_matrix(cmat):
-	cmat = pd.DataFrame(cmat, index = ['False','True'], columns = ['False','True'])
+	'''
+	Transfrom a confusion matrix from sklearn.metrics to a long-form 
+	data frame with observed and predicted labels. 
+	'''
+	cmat = pd.DataFrame(cmat, index=['False', 'True'], columns=['False', 'True'])
 	cmat.index.name = 'Observed'
 	cmat.columns.name = 'Predicted'
 	cmat.reset_index(inplace=True)
@@ -18,7 +22,18 @@ def format_confusion_matrix(cmat):
 	return cmat 
 
 def confusion_plot(observed, predicted, norm=True, model_names=None):
-	cmats = [confusion_matrix(observed, array_pred) for array_pred in predicted]
+	'''
+	Plot the results from multiple confusion matrices for the same observed data.
+
+	Arguments:
+		observed: array of values of dependent variable
+		predicted: list of arrays of predicted values from models 
+		norm: should the values in the confusion matrix cells be normalized by 
+			the sum of within-class observations. Defaults to True. 
+		model_names: list of names given to the models to appear in the legend 
+			of the plot. 
+	'''
+	cmats = [confusion_matrix(observed, pred) for pred in predicted]
 	
 	if norm:
 		cmats = [cmat / cmat.sum(axis=1).astype(float) for cmat in cmats]
@@ -26,6 +41,8 @@ def confusion_plot(observed, predicted, norm=True, model_names=None):
 	cmats = [format_confusion_matrix(cmat) for cmat in cmats]
 
 	if model_names is not None:
+		if len(model_names) != len(predicted):
+			raise Exception('List of model names should be of same length as the list of predicted values.')
 		for name, cmat in zip(model_names, cmats):
 			cmat[' '] = name 
 	else:
@@ -49,24 +66,26 @@ def confusion_plot(observed, predicted, norm=True, model_names=None):
 		ax1.set_ylim([0.0, cdata.query('Observed == "False"')['value'].max() + 1])
 	f.subplots_adjust(hspace=0.02)
 	plt.tight_layout() 
-	return 
 
-## ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– ##
-## toy example 
+## ----------------------------------------------------------------------------------------------- ##
 
-data = np.array([0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1])
-model1 = np.array([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0])
-model2 = np.array([1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0])
-model3 = np.array([0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0])
+if __name__ == == "__main__":
 
-c1 = confusion_matrix(data, model1)
-c2 = confusion_matrix(data, model2)
-c3 = confusion_matrix(data, model3)
+	# fake results 
+	data = np.array([0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1])
+	model1 = np.array([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0])
+	model2 = np.array([1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0])
+	model3 = np.array([0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0])
 
-preds = [model1, model2, model3]
-names = ['Model 1', 'Model 2', 'Model 3']
+	# confusion matrices
+	c1 = confusion_matrix(data, model1)
+	c2 = confusion_matrix(data, model2)
+	c3 = confusion_matrix(data, model3)
 
-print confusion_plot(data, preds, norm=False, model_names=names)
+	# build a plot to compare results 
+	preds = [model1, model2, model3]
+	names = ['Model 1', 'Model 2', 'Model 3']
+	confusion_plot(data, preds, norm=True, model_names=names)
 
-## ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– ##
-## ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– ##
+## ----------------------------------------------------------------------------------------------- ##
+## ----------------------------------------------------------------------------------------------- ##
